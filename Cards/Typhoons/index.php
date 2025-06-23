9ced677d358d4dc4bd5643df5a283e52f6294bc8
JTer22 <Jankrister96@gmail.com>
2025-06-23 22:53:30 +0800

    modified cards, added php files each

---
+++ Cards/Typhoons/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+$isAdmin = false;
+?>
+
+<head>
+<?php include("C:/xampp/htdocs/DisasterProj/shared/head.php"); ?>
+
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="style.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+<?php include("C:/xampp/htdocs/DisasterProj/shared/nav.php"); ?>
+
+    <h1>This is for Typhoon</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ shared/nav.php
@@ -0,0 +1,34 @@
+<div class="container-fluid shadow position-sticky top-0 z-1 bg-body">
+  <div class="container py-3 d-flex flex-row justify-content-between align-items-center">
+    
+ <div class="h5 m-0" style="font-family: 'Montserrat', sans-serif;">
+     <strong style="color: rgb(99, 19, 19);">Alisto<span style="color: #0858d1;">PH</span></strong>
+</div>
+
+ 
+    <div class="text-center me-auto ms-5">
+      <div class="h6 text-dark my-0">
+        <?php
+  echo isset($date) ? $date : '';
+  if (isset($isAdmin) && $isAdmin) {
+    echo "<br>" . (isset($greeting) ? $greeting : 'Hello') . " <span class='badge rounded-pill text-bg-primary'>" . (isset($Name) ? $Name : 'Guest') . "</span>";
+  }
+  ?>
+      </div>
+    </div>
+
+    <!-- Navbar ito -->
+    <div class="d-flex flex-row">
+      <div class="me-2">
+        <a class="text-decoration-none text-dark" href="#">Home</a>
+      </div>
+      <div class="me-2">
+        <a class="text-decoration-none text-dark" href="#">About</a>
+      </div>
+      <div class="me-2">
+        <a class="text-decoration-none text-dark" href="#">Login</a>
+      </div>
+    </div>
+    
+  </div>
+</div>
